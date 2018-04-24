b3d863103e2bc2b65502aabd9309d618f0805d45
Osvaldo Lopez <you@example.com>
2018-04-23 20:08:10 -0500

    commit inicial

---
+++ public_html/js/pastel.js
@@ -0,0 +1,26 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content2").fadeOut(1500);
+    },3000);
+});
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".content3").fadeOut(1500);
+    },3000);
+});
+
+$(document).ready(function() {   
+    setTimeout(function() {
+        $(".content4").fadeIn(1500);
+    },3000);
+});
